5776d7f2e923682099cd124372e3401c5f1daba1
Matthew Gall <me@matthewgall.dev>
2021-05-28 21:39:26 +0100

    Update abusedb provider to use hostnames, and do DNS lookups itself

--- providers/abusedb.js
+++ providers/abusedb.js
@@ -1,17 +1,28 @@
 import axios from "axios";
+import DNS from "./dns.js";
 
 const endpoint = 'https://api.findabuse.email/api/v1'
 
 const AbuseDB = {
-    lookup: async(addr) => {
+    lookup: async(hostname) => {
         // Check the cache
-        let cacheKey = `/abusedb/${Buffer.from(addr).toString('base64')}`;
+        let cacheKey = `/abusedb/${Buffer.from(hostname).toString('base64')}`;
         global.db.get(cacheKey, function(err, reply) {
             if (reply) {
                 return JSON.parse(reply)['data'];
             }
         });
 
+        // Now we resolve the hostname
+        let addr = [];
+        let types = ['A', 'AAAA'];
+
+        for (let r of types) {
+            let dns = await DNS.lookup(hostname, r);
+            addr = [...addr, ...dns];
+        }
+
+        // And lookup using abusedb
         const res = await axios.get(`${endpoint}/${addr.join(',')}`, {
             headers: {
                 "User-Agent": "phishing.fyi/servator",
