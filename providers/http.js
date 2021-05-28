0b2cbe0cb734dce6c6ad0c4d93517bac1b986609
Matthew Gall <me@matthewgall.dev>
2021-05-28 15:55:18 +0100

    Added redis caching to providers/http

--- providers/http.js
+++ providers/http.js
@@ -2,12 +2,27 @@ import axios from "axios";
 
 const Http = {
     lookup: async(url) => {
+        // Check the cache
+        let cacheKey = `/http/${Buffer.from(url).toString('base64')}`;
+        global.db.get(cacheKey, function(err, reply) {
+            if (reply) {
+                return JSON.parse(reply)['data'];
+            }
+        });
+
         const res = await axios.get(url, {
             headers: {
                 "User-Agent": "phishing.fyi/servator",
             },
         });
         let data = (await res).data;
+
+        // Save to cache
+        global.db.set(cacheKey, JSON.stringify({
+            'data': data
+        }));
+        global.db.expire(cacheKey, 604800);
+
         return Buffer.from(data).toString('base64');
     },
 };
